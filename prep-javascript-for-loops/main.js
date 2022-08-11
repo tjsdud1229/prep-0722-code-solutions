0839d1f3e85b883b412fd1ff3f09945adb237fe0
Tina Kim <tjsdud1229@gmail.com>
2022-08-11 23:26:36 +0000

    prep-javascript-loops-Tina Kim.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,16 @@
+// create your loops here.
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+let num = 0;
+for (let i = 0; i < 10; i++) {
+  console.log(num);
+  num += 2;
+
+}
+
+for (let i = 100; i >= 0; i--) {
+  console.log('Time till explosion: ' + i);
+}
